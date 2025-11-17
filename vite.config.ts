b321e3f7bd99628c0c7a815b6563a73ff3d11354
Polonskiy Igor <barlogfire@ya.ru>
2025-11-17 17:40:53 +0300

    fix some

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kameleoon_frontend_task/', // Очень важно для GitHub Pages!
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
